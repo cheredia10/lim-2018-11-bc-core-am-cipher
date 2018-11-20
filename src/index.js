e761dae909bfbf829433978035297312b0c27453
cheredia10 <cheredia.colegio@gmail.com>
2018-11-20 12:55:49 -0500

    BOTON CIFRADO MEDIO COMPLETO

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 
-// crenado la funcion del boton Cifrar
-encriptar.addEventListener('click', () => {
-  //Creando variables que almacenan los datos de los input
-  const offset= document.getElementById('clave');
-  const messege= document.getElementById('mensaje');
-  let cifrado=[];
+// creando el evento de Cifrado
+const buttonEncriptar=document.getElementById("encriptar")
+ 
+ buttonEncriptar.addEventListener('click', cifrar);
 
-  for(let i=0; i<messege.length; i++) { //recorrido de letras a cifrar//
-      let ubicacionCesar=(messege.toUpperCase().charCodeAt(i) - 65 + pareseIn(offset)) % 26 + 65;
+// creando la función Crifrar
+function cifrar(){
+
+  //Creando variables que almacenan los datos de los input
+  let offset= document.getElementById('clave').value;
+  let mensajito= document.getElementById('mensj').value;
+  let cifrado="";
+
+  for(let i=0; i<mensajito.length ; i++) { //recorrido de letras a cifrar//
+    let numberCodeAscii= mensajito.charCodeAt(i);//obtenemos el Código ASCII
+
+    if (numberCodeAscii>=65 && numberCodeAscii<=90){ // condición codigo ASCII (Valores Mayusculas)
+
+      let ubicacionCesar= (numberCodeAscii - 65 + parseInt(offset)) % 26 + 65;
       let palabraCifrada= String.fromCharCode(ubicacionCesar);
 
-    cifrado+= palabraCifrada; //acumular las letras cifradas//
+      cifrado+= palabraCifrada; //acumular las letras cifradas//
     }
-    console.log(cifrado);
-});
+  }
+   document.getElementById("Textoamostrar").innerHTML = cifrado
+}
+
